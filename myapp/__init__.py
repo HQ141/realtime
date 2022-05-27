e5c404f580ec505ccdb51a3453256a13bc003536
HQ141 <hamdan90@live.com>
2022-05-27 19:59:11 +0500

    first up

---
+++ myapp/__init__.py
@@ -0,0 +1,39 @@
+from flask import Flask,request,render_template
+import sqlite3
+import re
+from datetime import datetime
+app= Flask(__name__)
+def logger(request):
+	conn =sqlite3.connect('/usr/src/SQLite/New.db')
+	tmp=""
+	i=0
+	for he in request.headers:
+		if(i>5):
+			tmp=tmp+" "+he[1]
+		i=i+1
+	method=request.method
+	path=request.full_path
+	body=request.get_data(as_text=True)
+	dt=datetime.now()
+	malicious=malcheck(body,path)
+	conn.execute('''INSERT INTO TEST(REMOTE_ADDR,DATE_TIME,METHOD,PATH,HOST,USER_AGENT,ACCEPT,LANGUAGE,ENCODING,OTHER,BODY,SUS) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)''',(request.environ['REMOTE_ADDR'],dt,method,path,request.headers['Host'],request.headers['User-Agent'],request.headers['Accept'],request.headers['Accept-Language'],request.headers['Accept-Encoding'],tmp,body,malicious))
+	conn.commit()
+	conn.close()
+def malcheck(str1,str2):
+	sp_check=re.compile('[(|)|$|{|}|<|>|(|)|\|~|:|%7[B-E]|%5[B-D]|%3[A-C]|%3E|%2[2-6]]')
+	if(sp_check.search(str1)!=None):
+		return 1
+	if(sp_check.search(str2)!=None):
+		return 1
+	return 0
+	
+@app.route("/",methods=['GET'])
+def home():
+	logger(request)
+	return render_template("form.html")
+@app.route("/",methods=['POST'])
+def check():
+	logger(request)
+	return request.form['Name']
+if __name__=="__main__":
+	app.run(host="0.0.0.0")
---
+++ myapp/myapp.wsgi
@@ -0,0 +1,4 @@
+#!/usr/bin/python3.8
+import sys
+sys.path.insert(0,"/var/www/realtime/")
+from myapp import app as application
